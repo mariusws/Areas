0bf4898c98a30893eabfd2303b3474af81a52c8a
mariusws <mariusws@gmail.com>
2015-09-19 23:07:18 +0200

    added
    Added files

---
+++ ContactDetails/ContactDetailsAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MVCAssignement2.Areas.ContactDetails
+{
+    public class ContactDetailsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "ContactDetails";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ContactDetails_default",
+                "ContactDetails/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }, namespaces: new[] { "MVCAssignement2.Areas.ContactDetails.Controllers" }
+
+            );
+        }
+    }
+}
---
+++ ContactDetails/Controllers/ManagementController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAssignement2.Areas.ContactDetails.Controllers
+{
+    public class ManagementController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }        
+
+        [HttpPost]
+        public ActionResult Index(Models.ContactDetails details)
+        {
+            return View(details);
+        }        
+    }
+}
